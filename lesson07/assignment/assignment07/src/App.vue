df6769fb42ada868dd2901e9f7058a3e3493a54b
Rydavim <rydavim@gmail.com>
2023-12-21 10:47:23 -0500

    one more save state for restoration

--- lesson07/assignment/assignment07/src/App.vue
+++ lesson07/assignment/assignment07/src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <nav class="navbar bg-light justify-content-center">
       <ul class="nav">
-        <router-link v-for="(navLink, index) in navLinks" :key="index" tag="li" class="nav-item" active-class="active" :to="navLink.to" :exact="navLink.exact">
-          {{ navLink.txt }}
-        </router-link>
+        <router-link v-for="(navLink, index) in navLinks" :key="index" tag="li" class="nav-item" active-class="active" :to="navLink.to" :exact="navLink.exact"><a class="nav-link">{{ navLink.txt }}</a></router-link>
       </ul>
     </nav>
     <router-view></router-view>
@@ -13,17 +11,17 @@
 
 <script>
 export default {
-  name: 'App';
-},
-data () {
-  return {
-    // nav link array for population
-    navLinks: [
-      { to: '/', exact: true, txt: 'Home' },
-      { to: '/images', exact: false, txt: 'Images' },
-      { to: '/accordion', exact: false, txt: 'Accordion' },
-      { to: '/grid', exact: false, txt: 'Grid' }
-    ]
+  name: 'App',
+  data() {
+    return {
+      // nav link array for population
+      navLinks: [
+        { to: '/', exact: true, txt: 'Home' },
+        { to: '/images', exact: false, txt: 'Images' },
+        { to: '/accordion', exact: false, txt: 'Accordion' },
+        { to: '/grid', exact: false, txt: 'Grid' }
+      ]
+    };
   }
 }
 </script>
