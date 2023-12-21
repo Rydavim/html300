a02f08a9a264cce1b8b91bd6f1cf32d88a557784
Rydavim <rydavim@gmail.com>
2023-12-21 13:31:19 -0500

    components refactored

---
+++ lesson08/assignment/assignment07/src/components/ImageItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="toggleBorder">
+    <img :class="['img-fluid', { 'bordered': isBordered }]" :src="src" :alt="alt" :title="title">
+  </div>
+</template>
+
+<script>
+// import mixin
+import toggleBorderMixin from './toggleBorderMixin';
+
+export default {
+  name: 'ImageItem',
+  mixins: [toggleBorderMixin], // using mixin
+  // validation
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style>
+.bordered {
+  border: 2px solid black; /* example border styles */
+}
+</style>
--- lesson08/assignment/assignment07/src/components/ImagesContent.vue
+++ lesson08/assignment/assignment07/src/components/ImagesContent.vue
@@ -3,15 +3,17 @@
   <div class="container">
     <h1>Images</h1>
     <!-- iterate over img array to build gallery -->
-    <div v-for="(image, index) in images" :key="index">
-      <img class="img-fluid" :src="image.src" :alt="image.alt">
-    </div>
+    <image-item v-for="(image, index) in images" :key="index" :src="image.src" :alt="image.alt" :title="image.title" />
   </div>
 </template>
 
 <script>
+import ImageItem from './ImageItem.vue'; // import modular image component
 export default {
   name: 'ImageContent',
+  components: {
+    ImageItem  // add image item component
+  },
   props: {
     msg: String
   },
@@ -19,12 +21,12 @@ export default {
     return {
       // image array - iterate over images to add to template
       images: [
-        { src: require('../assets/DALLE-cabin-plans.png'), alt: 'simple blueprint sketches for a small winter cabin' },
-        { src: require('../assets/DALLE-holiday-TARDIS.png'), alt: 'low-poly illustration of the TARDIS from Doctor Who decorated for the holidays' },
-        { src: require('../assets/DALLE-jumping-fox.png'), alt: 'low-poly illustration of a fox jumping into snow' },
-        { src: require('../assets/DALLE-snow-woman.png'), alt: 'a woman in a long red winter coat standing in the snow' },
-        { src: require('../assets/DALLE-urban-gifts.png'), alt: 'illustration of a large pile of wrapped gifts stacked by a graffitied wall in a city' },
-        { src: require('../assets/DALLE-winter-bird.png'), alt: 'watercolor painting of a fluffy bird sitting on a snowy holly bush' }
+        { src: require('../assets/DALLE-cabin-plans.png'), alt: 'simple blueprint sketches for a small winter cabin', title: 'Cabin Plans' },
+        { src: require('../assets/DALLE-holiday-TARDIS.png'), alt: 'low-poly illustration of the TARDIS from Doctor Who decorated for the holidays', title: 'Holiday TARDIS' },
+        { src: require('../assets/DALLE-jumping-fox.png'), alt: 'low-poly illustration of a fox jumping into snow', title: 'Jumping Fox' },
+        { src: require('../assets/DALLE-snow-woman.png'), alt: 'a woman in a long red winter coat standing in the snow', title: 'Woman in Snow' },
+        { src: require('../assets/DALLE-urban-gifts.png'), alt: 'illustration of a large pile of wrapped gifts stacked by a graffitied wall in a city', title:'City Gifts' },
+        { src: require('../assets/DALLE-winter-bird.png'), alt: 'watercolor painting of a fluffy bird sitting on a snowy holly bush', title:'Winter Bird' }
       ]
     };
   }
