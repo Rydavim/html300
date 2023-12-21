4a1afa7f2bbe5bb233c89f4642f609847c8f1b11
Rydavim <rydavim@gmail.com>
2023-12-21 12:57:02 -0500

    files copied over from previous assignment

---
+++ lesson08/assignment/assignment07/src/components/GridContent.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- grid layout examples -->
+  <div class="container">
+    <h1>Grid Example</h1>
+    <div class="container">
+      <div class="row"> <!-- navigation -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><h2>Nav</h2></div>
+      </div>
+      <div class="row"> <!-- main layout -->
+        <div class="col-md-3 col-lg-4 col-xl-4 d-none d-md-block"><h2>Sidebar</h2></div>
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-8 col-xl-8"><h2>Content</h2></div>
+      </div>
+      <div class="row"> <!-- footer sections -->
+        <div class="col-xs-12 col-sm-12 col-md-4 col-l-4 col-xl-4"><h2>A</h2></div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-l-4 col-xl-4"><h2>B</h2></div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-l-4 col-xl-4"><h2>C</h2></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridContent',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ lesson08/assignment/assignment07/src/components/HomeContent.vue
@@ -0,0 +1,39 @@
+<template>
+<!-- main content -->
+  <div class="container">
+    <h1>Welcome to Kuhaku</h1>
+    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <!-- conditionally change button class based on popover state -->
+    <b-button id="popover-target" :class="isPopoverVisible ? 'btn-success' : 'btn-danger'" class="btn btn-lg" @click="togglePopover">
+      <!-- v-if v-else to update button text based on popover visibility -->
+      <span v-if="isPopoverVisible">Put It Down!</span>
+      <span v-else>Drink Me!</span>
+    </b-button>
+    <b-popover target="popover-target" triggers="click" placement="top">
+      <template #title>Why would you do that?</template>
+      Don't drink random things you find online!
+    </b-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeContent',
+  data() {
+    return {
+      isPopoverVisible: false,  // tracking popover visibility, default false
+    };
+  },
+  methods: {
+    togglePopover() {
+      this.isPopoverVisible = !this.isPopoverVisible; // toggle popover state
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
